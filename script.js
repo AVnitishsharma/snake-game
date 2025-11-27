38ee5bce691bee297d12fe36d6d1951b2924cd09
Nitish Sharma <ernitishsharma361@gmail.com>
2025-11-27 23:05:17 +0530

    snake move

--- script.js
+++ script.js
@@ -2,14 +2,63 @@ const board = document.querySelector('.board')
 const blockHeight = 50
 const blockWidth = 50
 
-const cols = Math.floor(board.clientHeight / blockHeight)
-const rows = Math.floor(board.clientWidth / blockWidth)
+const rows = Math.floor(board.clientHeight / blockHeight)
+const cols = Math.floor(board.clientWidth / blockWidth)
 
-for (let row = 0; row < rows; row++){
-  for(let col = 0; col < cols; col++){
+const blocks = [];
+const snake = [
+  {
+    x:1, y:3
+  }
+];
+let direction = ""
+
+for (let row = 0; row < rows ; row++) {
+  for(let col = 0; col < cols ; col++) {
     const block = document.createElement('div')
     block.classList.add("block")
     board.appendChild(block)
-    block.innerText =`${row}-${col}`
+    blocks[ `${row}-${col}` ] = block
   }
 }
+
+function render() {
+  snake.forEach(segment => {
+    blocks[ `${segment.x}-${segment.y}` ].classList.add("fill")
+  })
+}
+
+setInterval(() => {
+  let head = null
+
+  if (direction === "left") {
+    head = {x:snake[0].x, y:snake[0].y-1}
+  }else if(direction === "right") {
+    head = {x:snake[0].x, y:snake[0].y+1}
+  }else if(direction === "down") {
+    head = {x:snake[0].x+1, y:snake[0].y}
+  }else if(direction === "up") {
+    head = {x:snake[0].x-1, y:snake[0].y}
+  }
+
+  snake.forEach(segment => {
+    blocks[ `${segment.x}-${segment.y}` ].classList.remove("fill")
+  })
+
+  snake.unshift(head)
+  snake.pop()
+  
+  render()
+}, 400);
+
+addEventListener("keydown", (event) => {
+  if(event.key === "arrowUp"){
+    direction = "up"
+  }else if(event.key === "arrowRight"){
+    direction = "right"
+  }else if(event.key === "arrowleft"){
+    direction = "left"
+  }else if(event.key === "arrowDown"){
+    direction = "down"
+  }
+})yu
